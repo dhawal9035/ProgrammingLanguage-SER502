e20126c673914708d71eb84348f2599ba982de42
pankhiprasher <pprasher@asu.edu>
2016-04-25 11:30:01 -0700

    CHANGES IN RUNTIME

--- src/RunTime/RunTime.java
+++ src/RunTime/RunTime.java
@@ -8,9 +8,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Stack;
 
-/**
- * Created by Dhawal Soni on 4/23/2016.
- */
+
 public class RunTime {
     public static Stack<Integer> varStack = new Stack<>();
     public static HashMap<String,LinkedList<Integer>> symbolTable = new HashMap<>();
@@ -22,8 +20,6 @@ public class RunTime {
     }
 
     public static void startExecution(Scanner sc) throws Exception{
-        int a;
-        int b;
         while(sc.hasNext()){
             String s = sc.nextLine();
             String[] opCode = s.split(" ");
@@ -50,38 +46,24 @@ public class RunTime {
                     }
                     break;
                 case "DIV":
-                    a = varStack.pop();
-                    b = varStack.pop();
+                    int a = varStack.pop();
+                    int b = varStack.pop();
                     varStack.push(b/a);
                     break;
                 case "MUL":
-                    a = varStack.pop();
-                    b = varStack.pop();
-                    varStack.push(b*a);
+                    int c = varStack.pop();
+                    int d = varStack.pop();
+                    varStack.push(c*d);
                     break;
                 case "ADD":
-                    a = varStack.pop();
-                    b = varStack.pop();
-                    varStack.push(b+a);
+                    int e = varStack.pop();
+                    int f = varStack.pop();
+                    varStack.push(e+f);
                     break;
                 case "SUB":
-                    a = varStack.pop();
-                    b = varStack.pop();
-                    varStack.push(b-a);
-                    break;
-                case "IF":
-                    break;
-                case "LT":
-                    break;
-                case "GT":
-                    break;
-                case "GTE":
-                    break;
-                case "LTE":
-                    break;
-                case "EQ":
-                    break;
-                case "NE":
+                    int g = varStack.pop();
+                    int h = varStack.pop();
+                    varStack.push(h-g);
                     break;
                 case "Print":
                     System.out.print(varStack.pop());
@@ -91,6 +73,58 @@ public class RunTime {
                     break;
                 case "ENDS":
                     System.exit(0);
+                case "EQUAL":
+                    int i=varStack.pop();
+                    int j=varStack.pop();
+                    varStack.push(i==j);
+                    break;
+                case "GT":
+                    int k=varStack.pop();
+                    int l=varStack.pop();
+                    varStack.push(k>l);
+                    break;
+                case "LT":
+                    int m=varStack.pop();
+                    int n=varStack.pop();
+                    varStack.push(m<n);
+                    break;
+                case "LTE":
+                    int o=varStack.pop();
+                    int p=varStack.pop();
+                    varStack.push(o<=p);
+                    break;
+                case "GTE":
+                    int q=varStack.pop();
+                    int r=varStack.pop();
+                    varStack.push(q>=r);
+                    break;
+                case "NOTEQUALS":
+                    int s=varStack.pop();
+                    int t=varStack.pop();
+                    varStack.push(s!=t);
+                    break;
+                case "MOD":
+                    int u=varStack.pop();
+                    int v=varStack.pop();
+                    if(u==0.0){
+                        varStack.push(0);
+                    }
+                    else{
+                        varStack.push(u%v);
+                    }
+
+                    break;
+                case "ASSIGNMENT":
+                    int x=varStack.pop();
+                    int y=varStack.pop();
+                    varStack.push(x=y);
+                    break;
+                case "if":
+                    int op1=varStack.pop();
+                    int op2=varStack.pop();
+
+
+
                 default:
                     throw new IllegalArgumentException(opCode[0]+" Arguement is not defined");
             }
