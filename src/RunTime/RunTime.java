2400acfb380774100593e125ac0428f053fe404d
jreigel <justinreigel@yahoo.com>
2016-04-26 15:47:04 -0700

    Merged files, added skeleton for if/else

--- src/RunTime/RunTime.java
+++ src/RunTime/RunTime.java
@@ -12,39 +12,40 @@ import java.util.Stack;
  * Created by Dhawal Soni on 4/23/2016.
  */
 public class RunTime {
+    public static HashMap<String, LinkedList<Integer>> symbolTable = new HashMap<>();
     public static Stack<Integer> varStack = new Stack<>();
-    public static HashMap<String,LinkedList<Integer>> symbolTable = new HashMap<>();
-    public static void main(String args[]) throws Exception{
+
+    public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(new File(args[0]));
-        if(sc.nextLine().equalsIgnoreCase("Program Body begins")){
+        if (sc.nextLine().equalsIgnoreCase("Program Body begins")) {
             startExecution(sc);
         }
     }
 
-    public static void startExecution(Scanner sc) throws Exception{
+    public static void startExecution(Scanner sc) throws Exception {
         int a;
         int b;
-        while(sc.hasNext()){
+        while (sc.hasNext()) {
             String s = sc.nextLine();
             String[] opCode = s.split(" ");
-            switch(opCode[0]){
+            switch (opCode[0]) {
                 case "PUSH":
                     varStack.push(Integer.parseInt(opCode[1]));
                     break;
                 case "STORE":
-                    if(symbolTable.containsKey(opCode[1])){
+                    if (symbolTable.containsKey(opCode[1])) {
                         LinkedList<Integer> list = symbolTable.get(opCode[1]);
                         list.add(varStack.pop());
                     } else {
                         LinkedList<Integer> list = new LinkedList<>();
                         list.add(varStack.pop());
-                        symbolTable.put(opCode[1],list);
+                        symbolTable.put(opCode[1], list);
                     }
                     break;
                 case "LOAD":
-                    if(symbolTable.containsKey(opCode[1])){
+                    if (symbolTable.containsKey(opCode[1])) {
                         LinkedList<Integer> list = symbolTable.get(opCode[1]);
-                        varStack.push(list.get(list.size()-1));
+                        varStack.push(list.get(list.size() - 1));
                     } else {
                         throw new VariableNotDeclaredException("The variable has not been declared");
                     }
@@ -52,36 +53,22 @@ public class RunTime {
                 case "DIV":
                     a = varStack.pop();
                     b = varStack.pop();
-                    varStack.push(b/a);
+                    varStack.push(b / a);
                     break;
                 case "MUL":
                     a = varStack.pop();
                     b = varStack.pop();
-                    varStack.push(b*a);
+                    varStack.push(b * a);
                     break;
                 case "ADD":
                     a = varStack.pop();
                     b = varStack.pop();
-                    varStack.push(b+a);
+                    varStack.push(b + a);
                     break;
                 case "SUB":
                     a = varStack.pop();
                     b = varStack.pop();
-                    varStack.push(b-a);
-                    break;
-                case "IF":
-                    break;
-                case "LT":
-                    break;
-                case "GT":
-                    break;
-                case "GTE":
-                    break;
-                case "LTE":
-                    break;
-                case "EQ":
-                    break;
-                case "NE":
+                    varStack.push(b - a);
                     break;
                 case "Print":
                     System.out.print(varStack.pop());
@@ -89,10 +76,76 @@ public class RunTime {
                 case "PrintLN":
                     System.out.println(varStack.pop());
                     break;
+                case "EQUAL":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(a == b ? 1 : 0);
+                    break;
+                case "GT":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b > a ? 1 : 0);
+                    break;
+                case "LT":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b < a ? 1 : 0);
+                    break;
+                case "LTE":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b <= a ? 1 : 0);
+                    break;
+                case "GTE":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b >= a ? 1 : 0);
+                    break;
+                case "NOTEQUALS":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b != a ? 1 : 0);
+                    break;
+                case "MOD":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    if (a == 0.0) {
+                        varStack.push(0);
+                    } else {
+                        varStack.push(b % a);
+                    }
+
+                    break;
+                case "ASSIGNMENT":
+                    a = varStack.pop();
+                    b = varStack.pop();
+                    varStack.push(b = a);
+                    break;
+                case "if":
+                    a = varStack.pop();
+                    boolean conditional = a > 0;
+                    int num = sc.nextInt();
+                    if (conditional) {
+//                        if command is true, continue until 'ENDIF#'
+                        while (!sc.nextLine().equals("GO TO EndIF" + num)) {
+//                            process lines
+
+                        }
+//                            skip the lines until it reaches the outer if ending
+                        while (!sc.nextLine().equals("ENDIF" + num)) {
+//                            just skips the lines
+                        }
+                    } else {
+//                        else, continue until 'GO TO EndIF#'
+                    }
+                    num = sc.nextInt();
+                    while (!sc.nextLine().equals("Else Ends" + num)) {
+//                            process the lines
+                    }
                 case "ENDS":
                     System.exit(0);
                 default:
-                    throw new IllegalArgumentException(opCode[0]+" Arguement is not defined");
+                    throw new IllegalArgumentException(opCode[0] + " Arguement is not defined");
             }
         }
     }
